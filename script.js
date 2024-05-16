39c5f1d12da1e55c4228c398f48533d0d0099849
weihsia <153365050+weihsia@users.noreply.github.com>
2024-05-16 21:01:18 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -15,8 +15,3 @@ function openQuiz_v1() {
 function openQuiz_v2() {
     window.open("quiz_play_v2.html", "_self");
 }
-
-function exitQuiz() {
-    check = confirm("\nWARNING!\n\nExiting now will lose all your progress.\n\nExit now?");
-    if (check == true) toHome();
-}
