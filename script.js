5a71081a075d2582da4b71ba0cd0959e50307325
weihsia <153365050+weihsia@users.noreply.github.com>
2024-05-20 17:48:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,13 @@
 // JavaScript source code
 
 function toHome() {
-    window.open("index.html", "_self")
+    window.open("../index.html", "_self")
 }
 
 function back() {
     history.back();   //¦^¤W­¶
 }
 
-function openQuiz_v1() {
-    window.open("quiz_play.html", "_self");
-}
-
-function openQuiz_v2() {
-    window.open("quiz_play_v2.html", "_self");
+function openQuiz() {
+    window.open("quiz/quiz_play_v3.html", "_self");
 }
